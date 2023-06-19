6aa81b8606bb5233f6302b337ca9573145647e08
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-19 17:28:01 +0300

    Update RocketElement.js

--- src/components/RocketElement.js
+++ src/components/RocketElement.js
@@ -21,7 +21,7 @@ RocketElement.propTypes = {
     country: PropTypes.string.isRequired,
     rocket_name: PropTypes.string.isRequired,
     rocket_type: PropTypes.string.isRequired,
-    flickr_images: PropTypes.array.isRequired,
+    flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
     rocket_id: PropTypes.string.isRequired,
   }).isRequired,
 };
