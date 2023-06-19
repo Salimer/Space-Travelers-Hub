484c66bbd9467e6fe6d928d8641d81f67f7afa44
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-19 12:17:18 +0300

    Create rocketsSlice.js

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+});
+
+export default rocketsSlice.reducer;
