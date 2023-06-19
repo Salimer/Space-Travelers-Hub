ea7f0743e9937bd772bd2beb29628ea05086629d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-19 15:09:24 +0300

    Update rocketsSlice.js

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,12 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchRockets } from './thunks';
 
 const initialState = {
-
+  rockets: [],
+  isLoading: true,
+  error: undefined,
 };
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchRockets.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(fetchRockets.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.rockets = action.payload;
+    })
+    .addCase(fetchRockets.rejected, (state, action) => {
+      state.isLoading = false;
+      console.log(action)
+    })
+  }
 });
 
 export default rocketsSlice.reducer;
