6e13636afe229024a167f61b653e8d20f61cc532
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-19 15:09:22 +0300

    Create RocketsPage.js

---
+++ src/components/RocketsPage.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+const RocketsPage = () => {
+    const rockets = [];
+
+    useEffect(() => {
+        dispatch
+    })
+
+    return(
+        <div>rockets</div>
+    )
+}
