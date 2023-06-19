727c106b9f9dd9952de8bba02a122ff99aa756e3
Andrianarivo David <andrianarivo.david@gmail.com>
2023-06-19 12:00:04 +0300

    build navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  const links = [
+    {
+      path: "/",
+      name: "rockets",
+    },
+    {
+      path: "missions",
+      name: "missions",
+    },
+    {
+      path: "my-profile",
+      name: "my profile",
+    },
+  ];
+  return (
+    <nav>
+      <ul>
+        {links.map((link) => (
+          <li>
+            <NavLink to={link.path}>{link.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
