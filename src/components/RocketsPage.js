9905de492ca0280bc0cfbd7fe4e9cfca0f1ce643
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-19 15:32:28 +0300

    Update RocketsPage.js

--- src/components/RocketsPage.js
+++ src/components/RocketsPage.js
@@ -1,13 +1,36 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import fetchRockets from '../redux/rockets/thunks';
 
 const RocketsPage = () => {
-    const rockets = [];
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch
-    })
+  const { rockets, isLoading } = useSelector((store) => store.rockets);
 
-    return(
-        <div>rockets</div>
-    )
-}
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  return (
+    <Section>
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          <ul>
+            {rockets.map((rocket) => (
+              <li key={rocket.id}>{rocket.country}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </Section>
+  );
+};
+
+export default RocketsPage;
+
+const Section = styled.section`
+
+`;
