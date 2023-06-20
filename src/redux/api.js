4fd184bbeced3a1e187549894cad8a2950ba4528
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-20 12:22:29 +0300

    Update api.js

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,8 @@
 const baseUrl = 'https://api.spacexdata.com/v3';
 
 const MISSIONS = 'missions';
+const ROCKETS = 'rockets';
 
-export { MISSIONS };
+export { MISSIONS, ROCKETS };
 
 export default baseUrl;
