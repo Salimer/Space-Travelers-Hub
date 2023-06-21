fee07de5852f2dcfb19d6ff0244ad258103999c1
Andrianarivo David <andrianarivo.david@gmail.com>
2023-06-21 17:20:02 +0300

    update font-size

--- src/components/RocketElement.js
+++ src/components/RocketElement.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { reserveRocket, cancelReserveRocket } from '../redux/rockets/rocketsSlice';
+import { cancelReserveRocket, reserveRocket } from '../redux/rockets/rocketsSlice';
 
 const RocketElement = ({ rocket }) => {
   const dispatch = useDispatch();
@@ -24,7 +24,15 @@ const RocketElement = ({ rocket }) => {
           {rocket.reserved === true && <span className="reserved">Reserved</span>}
           {rocket.description}
         </p>
-        <button onClick={(e) => { handleClick(e, rocket.id); }} className={rocket.reserved === true ? 'cancel-btn' : 'reserve-btn'} type="button">{rocket.reserved === true ? 'Cancel Reservation' : 'Reserve Rocket'}</button>
+        <button
+          onClick={(e) => {
+            handleClick(e, rocket.id);
+          }}
+          className={rocket.reserved === true ? 'cancel-btn' : 'reserve-btn'}
+          type="button"
+        >
+          {rocket.reserved === true ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </button>
       </div>
     </Li>
   );
@@ -44,59 +52,59 @@ RocketElement.propTypes = {
 };
 
 const Li = styled.li`
-display: flex;
-padding: 1rem 0;
-
-.img {
-  img {
-    width: 15rem;
-  }
-}
-
-.details {
   display: flex;
-  gap: 0rem;
-  flex-direction: column;
-  margin-left: 1rem;
-  justify-content: center;
+  padding: 1rem 0;
 
-  .title {
-    font-size: 18px;
-    font-weight: 600;
-  }
-
-  p {
-    font-size: 12px;
-
-    .reserved {
-      font-size: 11px;
-      border-radius: .2rem;
-      margin-right:.5rem;
-      padding: 0.08rem 0.28rem;
-      background-color: #1aa1b9;
-      color: white;
-      font-weight: 500;
+  .img {
+    img {
+      width: 15rem;
     }
   }
 
-  .reserve-btn {
-    max-width: fit-content;
-    padding: .5rem;
-    background-color: #027bff;
-    cursor: pointer;
-    border-radius: .2rem;
-    color: white;
-    border: transparent;
-  }
+  .details {
+    display: flex;
+    gap: 0rem;
+    flex-direction: column;
+    margin-left: 1rem;
+    justify-content: center;
 
-  .cancel-btn {
-    max-width: fit-content;
-    padding: .5rem;
-    background-color: white;
-    cursor: pointer;
-    border-radius: .2rem;
-    color: #80878e;
-    border: 1px solid #a0a5ab;
+    .title {
+      font-size: 1.1rem;
+      font-weight: 600;
+    }
+
+    p {
+      font-size: 0.9rem;
+
+      .reserved {
+        font-size: 11px;
+        border-radius: .2rem;
+        margin-right: .5rem;
+        padding: 0.08rem 0.28rem;
+        background-color: #1aa1b9;
+        color: white;
+        font-weight: 500;
+      }
+    }
+
+    .reserve-btn {
+      max-width: fit-content;
+      padding: .5rem;
+      background-color: #027bff;
+      cursor: pointer;
+      border-radius: .2rem;
+      color: white;
+      border: transparent;
+    }
+
+    .cancel-btn {
+      max-width: fit-content;
+      padding: .5rem;
+      background-color: white;
+      cursor: pointer;
+      border-radius: .2rem;
+      color: #80878e;
+      border: 1px solid #a0a5ab;
+    }
   }
-}
 `;
