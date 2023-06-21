2902a578f6e87d9e211731ad490efd7a6b672fd7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-06-21 18:12:42 +0300

    Update ProfileRockets.test.js

--- src/tests/rockets/ProfileRockets.test.js
+++ src/tests/rockets/ProfileRockets.test.js
@@ -6,35 +6,35 @@ import ProfileRockets from '../../components/ProfileRockets';
 import rocketsReducer from '../../redux/rockets/rocketsSlice';
 
 const store = configureStore({
-    reducer: {
-      rockets: rocketsReducer, 
-    },
-  });
+  reducer: {
+    rockets: rocketsReducer,
+  },
+});
 
 test('renders ProfileRockets correctly', () => {
-    const rockets = [
-        {
-          id: 1,
-          rocket_name: 'Falcon 9',
-          description: 'A two-stage rocket designed and manufactured by SpaceX.',
-          image: 'falcon9.jpg',
-          reserved: false,
-        },
-        {
-            id: 2,
-            rocket_name: 'Falcon 10',
-            description: 'A two-stage rocket designed and manufactured by SpaceX.',
-            image: 'falcon10.jpg',
-            reserved: true,
-          },
-      ];
+  const rockets = [
+    {
+      id: 1,
+      rocket_name: 'Falcon 9',
+      description: 'A two-stage rocket designed and manufactured by SpaceX.',
+      image: 'falcon9.jpg',
+      reserved: false,
+    },
+    {
+      id: 2,
+      rocket_name: 'Falcon 10',
+      description: 'A two-stage rocket designed and manufactured by SpaceX.',
+      image: 'falcon10.jpg',
+      reserved: true,
+    },
+  ];
 
-    const { container } = render (
-        <Provider store={store}>
-            <ProfileRockets rockets={rockets}/>
-        </Provider>
-    );
+  const { container } = render(
+    <Provider store={store}>
+      <ProfileRockets rockets={rockets} />
+    </Provider>,
+  );
 
-    expect(container.firstChild).toMatchSnapshot();
-    expect(screen.getByText('My rockets')).toBeInTheDocument();
-})
+  expect(container.firstChild).toMatchSnapshot();
+  expect(screen.getByText('My rockets')).toBeInTheDocument();
+});
